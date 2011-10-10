7c2dae02965f97b977103f207c151ad1232961b6
jb <jeanbaptiste.verrey@gmail.com>
2011-10-10 21:55:04 +0100

    v0.10.10

--- Object/Attribute/Date.php
+++ Object/Attribute/Date.php
@@ -15,9 +15,50 @@ namespace CRUDsader\Object\Attribute {
         protected function _inputValid() {
             if (parent::_inputValid())
                 return true;
-            if(!preg_match('|[0-9]{2}/[0-9]{2}/[0-9]{4}|', $this->_inputValue))
+            if (!preg_match('|[0-9]{2}/[0-9]{2}/[0-9]{4}|', $this->_inputValue))
                 return 'invalid';
             return true;
+        }
+
+        /**
+         * when writing object from database
+         * @param type $value
+         */
+        public function setValueFromDatabase($value) {
+            if (\CRUDsader\Expression::isEmpty($value))
+                $this->_inputValue = new \CRUDsader\Expression\Nil();
+            else {
+                $ex = explode('-', $value);
+                switch (count($ex)) {
+                    case 3:
+                        $this->_inputValue = ($ex[2]=='00'?'':$ex[2]. '/') . ($ex[1]=='00'?'': $ex[1].'/')  . $ex[0];
+                        break;
+                    case 2:
+                        $this->_inputValue = $ex[1] . '/' . $ex[0];
+                        break;
+                    case 1:
+                        $this->_inputValue = $ex[0];
+                        break;
+                }
+            }
+        }
+
+        public function getValueForDatabase() {
+            if ($this->inputEmpty())
+                return new \CRUDsader\Expression\Nil;
+            $ex = explode('/', $this->_inputValue);
+            switch (count($ex)) {
+                case 3:
+                    return $ex[2] . '-' . $ex[1] . '-' . $ex[0];
+                    break;
+                case 2:
+                    return $ex[1] . '-' . $ex[0] . '-00';
+                    break;
+                case 1:
+                    return $ex[0] . '-00-00';
+                    break;
+            }
+            return new \CRUDsader\Expression\Nil;
         }
     }
 }
