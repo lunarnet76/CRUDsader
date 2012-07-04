210d1ffb380300c525c65585541a4532bddd20d1
jb <jb@xdesign365.com>
2012-07-04 12:24:23 +0100

    config loader

--- ArrayLoader/Yaml.php
+++ ArrayLoader/Yaml.php
@@ -16,16 +16,16 @@ namespace CRUDsader\ArrayLoader {
          *
          * @param array $options
          * @return array 
-         * @test test_load
+         * @test test_load,test_loadException
          */
         public function load(array $options){
             if(!isset($options['file']))
-                throw new YamlException('you must specify a file');
+                throw new ExtendedIniException('you must specify a file');
             $filePath=$options['file'];
             $section=isset($options['section'])?$options['section']:false;
             $lines = file($filePath);
             if ($lines === false)
-                throw new YamlException('file "' . $filePath . '" could not be read properly');
+                throw new ExtendedIniException('file "' . $filePath . '" could not be read properly');
             $configuration = array();
             $depths = array();
             $lastDepth = $depth = 0;
@@ -35,24 +35,27 @@ namespace CRUDsader\ArrayLoader {
                     case '#':break; // comments
                     case '[':// namespace
                         if (!preg_match('|^\[([^\:\]\s]*)(\:([^\]\s\:]*)){0,1}\]\s*$|', $line, $match))
-                            throw new YamlException('file "' . $filePath . '":' . ($lineNumber+1) . ' error :"' . $line . '" is not a proper namespace');
+                            throw new ExtendedIniException('file "' . $filePath . '":' . ($lineNumber+1) . ' error :"' . $line . '" is not a proper namespace');
 
                         if ($section && $namespace == $section) {
                             break 2;
                         }
                         $namespace = $match[1];
                         $configuration[$namespace] = array();
+                        // inheritance
                         if (isset($match[3])) {
                             if (!isset($configuration[$match[3]]))
-                                throw new YamlException('section "' . $namespace . '" cannot inherit from unexistant section "' . $match[3] . '"');
-                            $configuration[$namespace] = $configuration[$match[3]];
+                                throw new ExtendedIniException('section "' . $namespace . '" cannot inherit from unexistant section "' . $match[3] . '"');
+                            // copy the parent
+			   // pre($configuration[$match[3]]);
+                            $configuration[$namespace]= $this->unreference($configuration[$match[3]]);
                         }
                         break;
                     default:// config line
                         if (preg_match('|^(\s*)([^:=]*)([:=])(.*)\s*$|', $line, $match)) {// key:
                             $depth = strlen($match[1]);
 			    if($depth%4)
-				    throw new YamlException('depth separator must be 4 spaces and not '.($depth).' at line '.($lineNumber+1).':'.$match[0]);
+				    throw new ExtendedIniException('depth separator must be 4 spaces and not '.($depth).' at line '.($lineNumber+1).':'.$match[0]);
 			    $depth=$depth/4;
                             $name = $match[2];
                             if ($depth == 0) {// depth 0
@@ -62,9 +65,6 @@ namespace CRUDsader\ArrayLoader {
                             } else if ($depth == $lastDepth) {// same depth
                                 if (!isset($depths[$depth - 1][$name]))
                                     $depths[$depth - 1][$name] = array();
-				if(!isset($depths[$depth - 1]) || !is_array($depths[$depth - 1])){
-					throw new YamlException('error: should be at depth '.($depth - 1).' at line '.($lineNumber+1).':'.$line);
-				}
                                 $depths[$depth] = &$depths[$depth - 1][$name];
                             } else if ($depth > $lastDepth) {// >
                                 if (!isset($depths[$lastDepth][$name]))
@@ -81,8 +81,15 @@ namespace CRUDsader\ArrayLoader {
                 }
             }
             if ($section && !isset($configuration[$section]))
-                throw new YamlException('section "' . $section . '" does not exist');
+                throw new ExtendedIniException('section "' . $section . '" does not exist');
             return isset($configuration[$section])?$configuration[$section]:$configuration;
+        }
+        
+        protected function unreference(array $a){
+            $ret = array();
+            foreach($a as $k=>$v)
+                $ret[$k]=is_array($v)?$this->unreference($v):$v;
+            return $ret;
         }
     }
     class YamlException extends \CRUDsader\Exception{
