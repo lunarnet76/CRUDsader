572f4ff775302f644e0eb571ed0c46681c775772
jb <jb@xdesign365.com>
2012-07-05 10:31:41 +0100

    update xml map loader

--- Map/Loader/Xml.php
+++ Map/Loader/Xml.php
@@ -29,19 +29,9 @@ namespace CRUDsader\Map\Loader {
 			parent::__construct();
 			$this->_file = $this->_configuration->file;
 
-			$this->_cacheManager = \CRUDsader\Instancer::getInstance()->cache->void;
+			$this->_cacheManager = \CRUDsader\Instancer::getInstance()->cache;
 
 			$this->_cacheIndex = 'map_loader' . md5($this->_file);
-
-			if (false === $data = $this->_cacheManager->get($this->_cacheIndex)) {
-				if (!file_exists($this->_file))
-					throw new LoaderException('XML Map File "' . $this->_file . '" does not exist');
-				$this->_dom = simplexml_load_file($this->_file);
-				if (!$this->_dom)
-					throw new LoaderException('XML Map File "' . $this->_file . '" could not be loaded');
-				$this->_cache = false;
-			}else
-				$this->_cache = true;
 		}
 
 		/**
@@ -64,128 +54,132 @@ namespace CRUDsader\Map\Loader {
 		 */
 		public function getSchema(\CRUDsader\Block $defaults=null)
 		{
-			if (!$this->_cache) {
-				$ret = array('attributeTypes' => array(), 'classes' => array());
-				// attributeTypes
-				$attributeTypes = $this->_dom->attributeTypes->attributeType;
-				foreach ($attributeTypes as $attributeType) {
-					$alias = (string) $attributeType['alias'];
-					$ret['attributeTypes'][$alias] = array(
-					    'length' => (int) $attributeType['length'],
-					    'class' => (isset($attributeType['class']) ? ucfirst((string) $attributeType['class']) : $defaults->attributeType->class),
-					    'phpNamespace' => (isset($attributeType['phpNamespace']) ? ucfirst((string) $attributeType['phpNamespace']) : $defaults->attributeType->phpNamespace),
-					    'databaseType' => isset($attributeType['databaseType']) ? (string) $attributeType['databaseType'] : $defaults->attributeType->databaseType,
-					    'options' => isset($attributeType['options']) ? json_decode(str_replace('\'', '"', (string) $attributeType['options']), true) : $defaults->attributeType->options->toArray(),
-					);
-					$ret['attributeTypes'][$alias]['options']['length'] = (int) $attributeType['length'];
-				}
-				// classes
-				$classes = $this->_dom->classes->class;
-				foreach ($classes as $class) {
-					$name = (string) $class['name'];
-					// definition
-					$ret['classes'][$name] = array(
-					    'definition' => array(
-						'databaseTable' => isset($class['databaseTable']) ? (string) $class['databaseTable'] : $name,
-						'identity' => isset($class['identity']) ? explode(',', (string) $class['identity']) : array(),
-						'databaseIdField' => isset($class['databaseIdField']) ? (string) $class['databaseIdField'] : $defaults->idField,
-						'databaseFieldCount' => isset($class['databaseFieldCount']) ? (int) $class['databaseFieldCount'] : false,
-						'databaseIdFieldType' => isset($class['databaseIdFieldType']) ? (string) $class['databaseIdFieldType'] : false,
-						'attributeCount' => array('id' => false),
-						'phpClass' => isset($class['phpClass']) ? (string) $class['phpClass'] : false,
-					    ),
-					    'inherit' => false,
-					    'attributes' => array(),
-					    'attributesReversed' => array(),
-					    'associations' => array()
-					);
-					// inheritance
-					$parent = false;
-					if (isset($class['inherit'])) {
-						$ret['classes'][$name]['inherit'] = (string) $class['inherit'];
-					}
-					// attributes
-					$attributes = $class->attribute;
-					foreach ($attributes as $attribute) {
-						$attributeName = (string) $attribute['name'];
-						$json = isset($attribute['json']) ? ((string) $attribute['json']) : $defaults->attribute->json;
-						$ret['classes'][$name]['attributes'][$attributeName] = array(
-						    'required' => isset($attribute['required']) ? ((string) $attribute['required']) == 'true' : $defaults->attribute->required,
-						    'default' => isset($attribute['default']) ? (string) $attribute['default'] : null,
-						    'databaseField' => isset($attribute['databaseField']) ? (string) $attribute['databaseField'] : $attributeName,
-						    'type' => isset($attribute['type']) ? (string) $attribute['type'] : 'default',
-						    'searchable' => isset($attribute['searchable']) ? (string) $attribute['searchable'] : $defaults->attribute->searchable,
-						    'calculated' => isset($attribute['calculated']) ? (string) $attribute['calculated'] : false,
-						    'input' => isset($attribute['input']) ? ((string) $attribute['input']) == 'true' : $defaults->attribute->input,
-						    'html' => isset($attribute['html']) ? ((string) $attribute['html']) == 'true' : $defaults->attribute->html,
-						    'json' => $json == 'true' ? true : ($json == 'false' ? false : 'optional'),
-						    'listing' => isset($attribute['listing']) ? (string) $attribute['listing'] == 'true' : $defaults->attribute->listing
-						);
-						$ret['classes'][$name]['definition']['attributeCount'][$attributeName] = false;
-						$ret['classes'][$name]['attributesReversed'][$ret['classes'][$name]['attributes'][$attributeName]['databaseField']] = $attributeName;
-					}
-					$ret['classes'][$name]['definition']['abstract'] = empty($ret['classes'][$name]['attributes']);
-					// associations
-					$associations = $class->associated;
-					foreach ($associations as $association) {
 
-						$associationName = isset($association['name']) ? (string) $association['name'] : (string) $association['to'];
-						$ref = isset($association['reference']) ? (string) $association['reference'] : $defaults->associations->reference;
-						$to = (string) $association['to'];
-						$ret['classes'][$name]['associations'][$associationName] = array(
-						    'to' => $to,
-						    'reference' => $ref,
-						    'name' => isset($association['name']) ? (string) $association['name'] : false,
-						    'min' => isset($association['min']) ? (int) $association['min'] : $defaults->associations->min,
-						    'max' => isset($association['max']) ? (int) $association['max'] : $defaults->associations->max,
-						    'composition' => isset($association['composition']) ? ((string) $association['composition']) == 'true' : false,
-						    'databaseTable' => isset($association['databaseTable']) ? (string) $association['databaseTable'] : \CRUDsader\Map::getDatabaseAssociationTable(isset($association['name']) ? (string) $association['name'] : false, $to, $name),
-						    'databaseIdField' => isset($association['databaseIdField']) ? (string) $association['databaseIdField'] : $defaults->associations->databaseIdField,
-						    'internalField' => isset($association['internalField']) ? (string) $association['internalField'] : false,
-						    'externalField' => isset($association['externalField']) ? (string) $association['externalField'] : false,
-						    'inputPhpClass' => isset($association['inputPhpClass']) ? (string) $association['inputPhpClass'] : false
-						);
-						//  if ($ret['classes'][$name]['associations'][$associationName]['internalField'] == $ret['classes'][$name]['associations'][$associationName]['externalField'])
-						//    $ret['classes'][$name]['associations'][$associationName]['externalField'] .='2';
-					}
-				}
 
-				foreach ($ret['classes'] as $name => $infos) {
-					foreach ($infos['associations'] as $associationName => $association) {
-						switch ($association['reference']) {
-							case 'internal':
-								if (!$association['internalField'])
-									$association['internalField'] = $association['to'];
-								if (!$association['externalField'])
-									$association['externalField'] = $ret['classes'][$association['to']]['definition']['databaseIdField'];
-								$ret['classes'][$name]['associations'][$associationName]['internalField'] = $association['internalField'];
-								$ret['classes'][$name]['associations'][$associationName]['externalField'] = $association['externalField'];
-								$ret['classes'][$name]['definition']['attributeCount'][$association['internalField']] = true;
-								$ret['classes'][$name]['attributesReversed'][$association['internalField']] = $association['internalField'];
+			if (!file_exists($this->_file))
+				throw new LoaderException('XML Map File "' . $this->_file . '" does not exist');
+			$this->_dom = simplexml_load_file($this->_file);
+			if (!$this->_dom)
+				throw new LoaderException('XML Map File "' . $this->_file . '" could not be loaded');
 
-								break;
-							case 'external':
-								if (empty($association['internalField']))
-									$association['internalField'] = $ret['classes'][$name]['definition']['databaseIdField'];
-								if (!$association['externalField'])
-									$association['externalField'] = $name;
-								$ret['classes'][$name]['associations'][$associationName]['internalField'] = $association['internalField'];
-								$ret['classes'][$name]['associations'][$associationName]['externalField'] = $association['externalField'];
-								$ret['classes'][$association['to']]['definition']['attributeCount'][$association['externalField']] = true;
-								$ret['classes'][$association['to']]['attributesReversed'][$association['externalField']] = $association['externalField'];
-								break;
-						}
-					}
-				}
 
-				// pre($ret['classes']);
-				// to avoid cache problems
-				unset($this->_dom);
-				
-				$this->_cacheManager->set($this->_cacheIndex,$ret);
-			}else {
-				$ret = $this->_cacheManager->get($this->_cacheIndex);
+			$ret = array('attributeTypes' => array(), 'classes' => array());
+			// attributeTypes
+			$attributeTypes = $this->_dom->attributeTypes->attributeType;
+			foreach ($attributeTypes as $attributeType) {
+				$alias = (string) $attributeType['alias'];
+				$ret['attributeTypes'][$alias] = array(
+				    'length' => (int) $attributeType['length'],
+				    'class' => (isset($attributeType['class']) ? ucfirst((string) $attributeType['class']) : $defaults->attributeType->class),
+				    'phpNamespace' => (isset($attributeType['phpNamespace']) ? ucfirst((string) $attributeType['phpNamespace']) : $defaults->attributeType->phpNamespace),
+				    'databaseType' => isset($attributeType['databaseType']) ? (string) $attributeType['databaseType'] : $defaults->attributeType->databaseType,
+				    'options' => isset($attributeType['options']) ? json_decode(str_replace('\'', '"', (string) $attributeType['options']), true) : $defaults->attributeType->options->toArray(),
+				);
+				$ret['attributeTypes'][$alias]['options']['length'] = (int) $attributeType['length'];
 			}
+			// classes
+			$classes = $this->_dom->classes->class;
+			foreach ($classes as $class) {
+				$name = (string) $class['name'];
+				// definition
+				$ret['classes'][$name] = array(
+				    'definition' => array(
+					'databaseTable' => isset($class['databaseTable']) ? (string) $class['databaseTable'] : $name,
+					'identity' => isset($class['identity']) ? explode(',', (string) $class['identity']) : array(),
+					'databaseIdField' => isset($class['databaseIdField']) ? (string) $class['databaseIdField'] : $defaults->idField,
+					'databaseFieldCount' => isset($class['databaseFieldCount']) ? (int) $class['databaseFieldCount'] : false,
+					'databaseIdFieldType' => isset($class['databaseIdFieldType']) ? (string) $class['databaseIdFieldType'] : false,
+					'attributeCount' => array('id' => false),
+					'phpClass' => isset($class['phpClass']) ? (string) $class['phpClass'] : false,
+				    ),
+				    'inherit' => false,
+				    'attributes' => array(),
+				    'attributesReversed' => array(),
+				    'associations' => array()
+				);
+				// inheritance
+				$parent = false;
+				if (isset($class['inherit'])) {
+					$ret['classes'][$name]['inherit'] = (string) $class['inherit'];
+				}
+				// attributes
+				$attributes = $class->attribute;
+				foreach ($attributes as $attribute) {
+					$attributeName = (string) $attribute['name'];
+					$json = isset($attribute['json']) ? ((string) $attribute['json']) : $defaults->attribute->json;
+					$ret['classes'][$name]['attributes'][$attributeName] = array(
+					    'required' => isset($attribute['required']) ? ((string) $attribute['required']) == 'true' : $defaults->attribute->required,
+					    'default' => isset($attribute['default']) ? (string) $attribute['default'] : null,
+					    'databaseField' => isset($attribute['databaseField']) ? (string) $attribute['databaseField'] : $attributeName,
+					    'type' => isset($attribute['type']) ? (string) $attribute['type'] : 'default',
+					    'searchable' => isset($attribute['searchable']) ? (string) $attribute['searchable'] : $defaults->attribute->searchable,
+					    'calculated' => isset($attribute['calculated']) ? (string) $attribute['calculated'] : false,
+					    'input' => isset($attribute['input']) ? ((string) $attribute['input']) == 'true' : $defaults->attribute->input,
+					    'html' => isset($attribute['html']) ? ((string) $attribute['html']) == 'true' : $defaults->attribute->html,
+					    'json' => $json == 'true' ? true : ($json == 'false' ? false : 'optional'),
+					    'listing' => isset($attribute['listing']) ? (string) $attribute['listing'] == 'true' : $defaults->attribute->listing
+					);
+					$ret['classes'][$name]['definition']['attributeCount'][$attributeName] = false;
+					$ret['classes'][$name]['attributesReversed'][$ret['classes'][$name]['attributes'][$attributeName]['databaseField']] = $attributeName;
+				}
+				$ret['classes'][$name]['definition']['abstract'] = empty($ret['classes'][$name]['attributes']);
+				// associations
+				$associations = $class->associated;
+				foreach ($associations as $association) {
+
+					$associationName = isset($association['name']) ? (string) $association['name'] : (string) $association['to'];
+					$ref = isset($association['reference']) ? (string) $association['reference'] : $defaults->associations->reference;
+					$to = (string) $association['to'];
+					$ret['classes'][$name]['associations'][$associationName] = array(
+					    'to' => $to,
+					    'reference' => $ref,
+					    'name' => isset($association['name']) ? (string) $association['name'] : false,
+					    'min' => isset($association['min']) ? (int) $association['min'] : $defaults->associations->min,
+					    'max' => isset($association['max']) ? (int) $association['max'] : $defaults->associations->max,
+					    'composition' => isset($association['composition']) ? ((string) $association['composition']) == 'true' : false,
+					    'databaseTable' => isset($association['databaseTable']) ? (string) $association['databaseTable'] : \CRUDsader\Map::getDatabaseAssociationTable(isset($association['name']) ? (string) $association['name'] : false, $to, $name),
+					    'databaseIdField' => isset($association['databaseIdField']) ? (string) $association['databaseIdField'] : $defaults->associations->databaseIdField,
+					    'internalField' => isset($association['internalField']) ? (string) $association['internalField'] : false,
+					    'externalField' => isset($association['externalField']) ? (string) $association['externalField'] : false,
+					    'inputPhpClass' => isset($association['inputPhpClass']) ? (string) $association['inputPhpClass'] : false
+					);
+					//  if ($ret['classes'][$name]['associations'][$associationName]['internalField'] == $ret['classes'][$name]['associations'][$associationName]['externalField'])
+					//    $ret['classes'][$name]['associations'][$associationName]['externalField'] .='2';
+				}
+			}
+
+			foreach ($ret['classes'] as $name => $infos) {
+				foreach ($infos['associations'] as $associationName => $association) {
+					switch ($association['reference']) {
+						case 'internal':
+							if (!$association['internalField'])
+								$association['internalField'] = $association['to'];
+							if (!$association['externalField'])
+								$association['externalField'] = $ret['classes'][$association['to']]['definition']['databaseIdField'];
+							$ret['classes'][$name]['associations'][$associationName]['internalField'] = $association['internalField'];
+							$ret['classes'][$name]['associations'][$associationName]['externalField'] = $association['externalField'];
+							$ret['classes'][$name]['definition']['attributeCount'][$association['internalField']] = true;
+							$ret['classes'][$name]['attributesReversed'][$association['internalField']] = $association['internalField'];
+
+							break;
+						case 'external':
+							if (empty($association['internalField']))
+								$association['internalField'] = $ret['classes'][$name]['definition']['databaseIdField'];
+							if (!$association['externalField'])
+								$association['externalField'] = $name;
+							$ret['classes'][$name]['associations'][$associationName]['internalField'] = $association['internalField'];
+							$ret['classes'][$name]['associations'][$associationName]['externalField'] = $association['externalField'];
+							$ret['classes'][$association['to']]['definition']['attributeCount'][$association['externalField']] = true;
+							$ret['classes'][$association['to']]['attributesReversed'][$association['externalField']] = $association['externalField'];
+							break;
+					}
+				}
+			}
+
+			// pre($ret['classes']);
+			// to avoid cache problems
+			unset($this->_dom);
+
 			return $ret;
 		}
 	}
