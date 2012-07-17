7774ef38283bcbd7949722ebb70809f2698061a5
jb <jb@xdesign365.com>
2012-07-17 15:59:55 +0100

    timestmap

--- Object/Attribute/Timestamp.php
+++ Object/Attribute/Timestamp.php
@@ -16,10 +16,12 @@ namespace CRUDsader\Object\Attribute {
 		{
                         if(preg_match('|^[0-9]*$|',$this->_value))
                                 return true;
-			/*if (true!== $error = parent::isValid())
-				return $error;*/
-                        $t = preg_match('|^([0-9]{2})/([0-9]{2})/([0-9]{4})(?:\s([0-9]{2}\:[0-9]{2}))?(?:\s[0-9]{4})?$|', $this->_value,$match);
-                        if (!$t)
+                        // date + hours
+                        $t1 = preg_match('|^([0-9]{2})/([0-9]{2})/([0-9]{4})(?:\s([0-9]{2}:[0-9]{2})?$|', $this->_value,$match);
+                     
+                        $t2 = preg_match('|^([0-9]{4})-([0-9]{2})-([0-9]{2})(.*)$|', $this->_value,$match);
+                        
+                        if (!$t1 && !$t2)
 				return 'error.invalid';
                         $date = $match[3].'-'.$match[2].'-'.$match[1];
                         if(isset($this->_options['inTheFuture']) && $this->_options['inTheFuture'] && strtotime($date)<time())
@@ -62,13 +64,15 @@ namespace CRUDsader\Object\Attribute {
 		{
 			if($this->_value instanceof \CRUDsader\Expression)
 				return $this->_value;
+                        
 			if ($this->isEmpty())
 				return null;
 			
 			if(ctype_digit($this->_value))
 				return date('Y-m-d H:i:s',$this->_value);
-			if(preg_match('|^([0-9]{2})/([0-9]{2})/([0-9]{4})(?:\s([0-9]{2}\:[0-9]{2}))?(?:\s[0-9]{4})?$|',$this->_value,$match)){
-				
+                        
+                        
+			if(preg_match('|^([0-9]{2})/([0-9]{2})/([0-9]{4})(?:\s([0-9]{2}\:[0-9]{2}))?$|',$this->_value,$match)){
 				return $match[3].'-'.$match[2].'-'.$match[1].' '.(isset($match[4])?$match[4]:'');
 			}
 			$ex = explode('/', (string)$this->_value);
