bfa6146f82641007278deb0dea7de95d2d489d7d
jb <jb@xdesign365.com>
2012-07-17 10:19:27 +0100

    timestamp

--- Object/Attribute/Timestamp.php
+++ Object/Attribute/Timestamp.php
@@ -16,7 +16,7 @@ namespace CRUDsader\Object\Attribute {
 		{
 			if (true!== $error = parent::isValid())
 				return $error;
-                        $t = preg_match('|([0-9]{2})/([0-9]{2})/([0-9]{4})|', $this->_value,$match);
+                        $t = preg_match('|([0-9]{2})/([0-9]{2})/([0-9]{4})(\s[0-9]{2}\:[0-9]{2}:[0-9]{2}\s0000)?|', $this->_value,$match);
                         if (!$t)
 				return 'error.invalid';
                         $date = $match[3].'-'.$match[2].'-'.$match[1];
