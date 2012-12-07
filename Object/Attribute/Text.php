70b31f1b9a508abae07d48bf2635293985e743f6
jb <jeanbaptiste.verrey@gmail.com>
2012-12-07 16:37:55 +0100

    #attribute text nl2br

--- Object/Attribute/Text.php
+++ Object/Attribute/Text.php
@@ -6,15 +6,20 @@
  * @since       0.10.6
  */
 namespace CRUDsader\Object\Attribute {
-    class Text extends \CRUDsader\Object\Attribute\String {
-        public function toInput() {
-            if (isset($this->_htmlAttributes['value']))
-                unset($this->_htmlAttributes['value']);
-            return '<textarea ' . $this->getHtmlAttributesToHtml() . '>' . (isset($this->_value) ? $this->_value : '') . '</textarea>';
+        class Text extends \CRUDsader\Object\Attribute\String {
+
+                public function toInput() {
+                        if (isset($this->_htmlAttributes['value']))
+                                unset($this->_htmlAttributes['value']);
+                        return '<textarea ' . $this->getHtmlAttributesToHtml() . '>' . (isset($this->_value) ? $this->_value : '') . '</textarea>';
+                }
+
+                public function formatForDatabase($value) {
+                        return filter_var($value, FILTER_SANITIZE_STRING);
+                }
+
+                public function toHtml() {
+                        return nl2br($this->_Value);
+                }
         }
-        
-	public function formatForDatabase($value) {
-            return filter_var($value, FILTER_SANITIZE_STRING);
-        }
-    }
 }
