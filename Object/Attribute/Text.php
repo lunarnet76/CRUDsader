ccb8b340b3f8c25bde7c53dafda8aebcf4969545
jb <jeanbaptiste.verrey@gmail.com>
2012-12-07 16:45:27 +0100

    #again ...

--- Object/Attribute/Text.php
+++ Object/Attribute/Text.php
@@ -20,7 +20,7 @@ namespace CRUDsader\Object\Attribute {
 
                 public function toHtml() {
                         $ret = parent::toHtml();
-                        return nl2br($this->_value);
+                        return nl2br($ret);
                 }
         }
 }
